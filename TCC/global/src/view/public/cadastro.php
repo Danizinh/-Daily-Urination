8d2eb1b971c148119d7cc38f2b85de6ae71c8b69
Danizinh <danimaltoc@outlook.com>
2023-09-09 22:24:39 -0300

    adding login and registration designer

---
+++ TCC/global/src/view/public/cadastro.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="../src/styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../src/styles/style.css">
+    <title>Faça login</title>
+</head>
+
+<body>
+    <div class="card-container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Cadastro</h1>
+            </div>
+            <form class="card-form" action="" method="POST">
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="text" name="name" placeholder="Gabriela" id="emailForm" autocomplete="off" required>
+                </div>
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="text" placeholder="email@email.com" id="emailForm" autocomplete="off" required>
+                </div>
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="date" placeholder="email@email.com" id="emailForm" autocomplete="off" required>
+                </div>
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="password" name="password" placeholder="**********" id="passwordForm" autocomplete="off" required>
+                </div>
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="password" name="confimation" placeholder="**********" id="passwordForm" autocomplete="off" required>
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
+            <div class="card-footer">
+                Não tem uma conta?<a href="../public/login.php"> Faça seu login.</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
